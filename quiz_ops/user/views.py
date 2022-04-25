58ef00b1b8d92209bf98520fbe45c43b2c7f9f5b
Ashsenior <ashutoshjunior@gmail.com>
2022-04-25 23:32:54 +0530

    edited some variable names

--- quiz_ops/user/views.py
+++ quiz_ops/user/views.py
@@ -9,34 +9,38 @@ from django.http import JsonResponse
 class QuizResultView(APIView):
     def post(self,request,format=None):
         # Getting Data ready
-        data = request.data[0]
-        summary = data.get('summary')
-        detail = data.get('detail')
+        quiz_data = request.data[0]
+        quiz_summary = quiz_data.get('summary')
+        quiz_detail = quiz_data.get('detail')
         # Calculating number of correct and wrong attempts 
         no_correct_attempt = 0
         no_wrong_attempt = 0
         score_percentage = 0
-        for q in detail:
-            question = Question.objects.get(id=q.get('question_id'))
-            options = Options.objects.filter(question=question)
-            if options[q.get('user_answer_id')].option_flag:
+        # Iterate through each question in the request
+        for quiz_question in quiz_detail:
+            question = Question.objects.get(id=quiz_question.get('question_id'))
+            # Get the list of options through question_id
+            option_list = Options.objects.filter(question=question)
+            # If the chosen option in the option list is correct then
+            if option_list[quiz_question.get('user_answer_id')-1].option_flag:
                 no_correct_attempt += 1
+                # Add the weight of the question to score_percentage
                 score_percentage += question.weight
             else :
                 no_wrong_attempt += 1
         # Saving UserQuizSummary
-        user_quiz_summary = UserQuizSummary(user=summary.get('user'),
-                                            quiz_name=summary.get('quiz_name'),
-                                            quiz_date=summary.get('quiz_date'),
+        user_quiz_summary = UserQuizSummary(user=quiz_summary.get('user'),
+                                            quiz_name=quiz_summary.get('quiz_name'),
+                                            quiz_date=quiz_summary.get('quiz_date'),
                                             no_correct_attempt=no_correct_attempt,
                                             no_wrong_attempt=no_wrong_attempt)
         user_quiz_summary.save()
-        # Saving UserQuizDetail
-        for q in detail:
-            question = Question.objects.get(id=q.get('question_id'))
+        # Iterate through each qustion in the detail
+        for quiz_question in quiz_detail:
+            question = Question.objects.get(id=quiz_question.get('question_id'))
             user_quiz_detail = UserQuizDetail(quiz_id=user_quiz_summary,
                                               question_id=question,
-                                              user_answer=q.get('user_answer_id'))
+                                              user_answer=quiz_question.get('user_answer_id'))
             user_quiz_detail.save()
         return JsonResponse({'score_percentage':score_percentage})
             
